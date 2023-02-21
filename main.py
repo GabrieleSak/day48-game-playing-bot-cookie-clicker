4157439ed9b16cffaec1cc7e9d1077e5a7358843
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-02-21 11:13:00 +0200

    Print upgrades that are afffordable

--- main.py
+++ main.py
@@ -13,4 +13,19 @@ driver = webdriver.Chrome(service=service, options=options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, "cookie")
-cookie.click()
+money = driver.find_element(By.ID, "money")
+
+for i in range(150):
+    cookie.click()
+
+store = driver.find_elements(By.CSS_SELECTOR, "#store div:not(.grayed) b")
+
+items = {}
+for i in range(0, len(store)):
+    item = store[i].text.split(" - ")
+    items[i] = {
+        "name": item[0],
+        "price": item[1],
+    }
+
+print(items)
